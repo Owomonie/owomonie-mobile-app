b6a2cb8876c979f2225f58442b648c873a2207c6
yommexg <hydrokidb@gmail.com>
2025-01-31 20:48:43 +0000

    Balance Card update

---
+++ components/Home/acccountTitle.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeAcccountTitle = () => {
+  return (
+    <View>
+      <Text>HomeAcccountTitle</Text>
+    </View>
+  );
+};
+
+export default HomeAcccountTitle;
+
+const styles = StyleSheet.create({});
