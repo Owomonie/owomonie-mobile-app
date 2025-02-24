865ae1fa6ae31360bf502de3973d351428eff3b6
yommexg <hydrokidb@gmail.com>
2025-02-24 16:36:14 +0000

    update balance card color

--- components/Home/balanceCard.tsx
+++ components/Home/balanceCard.tsx
@@ -84,7 +84,7 @@ export default HomeBalanceCard;
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: "black",
+    backgroundColor: "red",
     borderWidth: 1,
     borderRadius: 12,
     overflow: "hidden",
